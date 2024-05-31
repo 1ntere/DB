e9ab79aaeedb08bec06a74f36e47fc56424cea6c
1ntere <kwonhc9337java@gmail.com>
2024-05-31 12:47:34 +0900

    function

---
+++ SQL_개념정리/02_함수.sql
@@ -0,0 +1,207 @@
+/*
+함수
+
+컬럼값이나 지정된 값을 읽어 연산한 결과를 반환하는 것
+
+단일 행 함수     : N 개의 행의 커럶 값을 전달하여 N 개의 결과가 반환
+
+그룹 함수        : N 개의 행의 컬럼 값을 전달하여 1개의 결과가 반환
+                  (그룹의 수가 늘어나면 그룹의 수 만큼 결과를 반환)
+
+함수는 SELECT절, WHERE절, ORDER BY절, HAVING절에서 사용 가능
+
+SELECT절 : 새로운 테이블에 표시가 될 행을 정리해 놓은 컬럼
+            찾는 기준이 아닌, 눈으로 표기되는 총 합, 계산 결과 등에 대한 것을 정리해놓은 컬럼
+            -> 정리해서 보여주자!
+WHERE절 : 찾는 조건을 작성해줌
+            길이, 크기, 결과가 조건에 부합하는 결과만 볼 수 있도록 설정
+            -> 해당 행에서 내가 원하는 조건에 부합하는 값 만 보여줘!
+*/
+
+/*****단일행 함수*****/
+--문자열 관련 함수
+--LENGTH(문자열|컬럼명) : 문자열의 길이 반환
+SELECT 'HELLO WORLD', LENGTH('HELLO WORLD')
+                    --LENGTH('HELLO WORLD') : 띄어쓰기 포함하여 11글자라고 표시됨
+FROM DUAL;
+
+--EMPLOYEE 테이블에서 사원명, 이메일, 이메일 길이 조회
+--이메일 길이 오름차순(작은 것부터 큰 순서대로) 조회
+SELECT EMP_NAME, EMAIL, LENGTH(EMAIL) AS "이메일 길이"
+FROM EMPLOYEE
+WHERE LENGTH(EMAIL) <= 12
+    --LENGTH(EMAIL) <= 12 : 12길이보다 이하인 이메일만 조회
+ORDER BY "이메일 길이";
+--ORDER BY "이메일 길이" : 오름차순으로 조회
+
+--INSTR(문자열|컬럼명, '찾을 문자열' [,찾는 것을 시작할 위치 [, 순번] ] )
+
+--찾을 시작 위치부터 지정된 순번째 찾은 문자열의 시작 위치를 반환
+
+--LIKE와 INSTR의 차이점
+--LIKE : @가 들어있는지 검색
+--INSTR : @가 몇번째에 있는지 검색
+
+--문자열에서 맨 앞에 있는 'B'의 위치를 조회
+SELECT 'AABAACAABBAA', INSTR('AABAACAABBAA', 'B')--5 : B가 첫 번째로 나타나는 위치는 5이다.
+                     --INSTR('AABAACAABBAA', 'B') : B가 문자열에서 처음 나타나는 위치
+FROM DUAL;--5
+   --DUAL : 존재하지 않는 테이블로 어떤 값을 테스트할 때 사용
+
+--문자열에서 5부터 검색해서 첫 번째로 찾은 'B'의 위치 조회
+SELECT 'AABAACAABBAA', INSTR('AABAACAABBAA', 'B', 5)--9 : B가 두 번째로 나타나는 위치는 9이다.
+                     --INSTR('AABAACAABBAA', 'B', 5) : 5번째 자리 이후에 처음 찾은 'B'의 위치를 조회
+                     --INSTR('AABAACAABBAA', 'B') : 처음부터 검색해서 처음 찾은 'B'의 위치를 조회
+FROM DUAL;--9
+
+--만약에 AABAACAABBAA가 아니라 AAB AAC AA BB AA였다면 --12가 나옴
+SELECT 'AAB AAC AA BB AA', INSTR('AAB AAC AA BB AA', 'B', 5)
+FROM DUAL;--12가 나옴
+
+--    문자열에서 5부터 검색해서 두 번째로 찾은 'B'의 위치 조회
+SELECT 'AABAACAABBAA', INSTR('AABAACAABBAA', 'B', 5, 2)--10 : B가 세 번째로 나타나는 위치는 10이다.
+FROM DUAL;--10
+
+--SUBSTR(문자열|컬럼명, 시작위치 [,길이])
+
+--문자열을 시작 위치부터 지정된 길이만큼 잘라내서 반환
+--길이를 작성하지 않은 경우 시작 위치부터 끝까지 잘라내서 반환
+
+--시작 위치, 자를 길이 지정
+SELECT SUBSTR('ABCDE', 2, 3)
+                     --2 : 2번째 자리에 존재하는 B부터 시작해서
+                        --3 : 3개만 가지고 오겠다
+FROM DUAL;--BCD
+
+--시작 위치만 지정하고, 자를 길이는 미지정
+SELECT SUBSTR('ABCDE', 4)
+                     --4 : 4번째 자리에 존재하는 D부터 시작해서 마지막까지 모두 가지고 오겠다
+FROM DUAL;--DE
+
+--시작위치의 부호를 - 로 설정하면 뒤에서 부터 가져옴
+--앞에서부터 값을 가지고 올 때는 + 로 작성해주지만
+--뒤에서부터 값을 가지고 오길 원할 때는 - 로 작성해주면 됨
+SELECT SUBSTR('ABCDE', -3)
+                     ---3 : 뒤에서 부터 3자리를 가져옴
+FROM DUAL;--CDE
+
+--EMPLOYEE 테이블에서 사원명, 이메일 아이디를 가져오는데
+--이메일 아이디에서 @ 앞에 있는 아이디만 가지고 오고, 이메일 아이디를 오름차순 조회
+SELECT EMP_NAME, SUBSTR(EMAIL, 1, INSTR(EMAIL, '@')-1) AS "이메일 아이디"
+               --SUBSTR(EMAIL, 1) : EMAIL을 모두 출력함
+                                --INSTR(EMAIL, '@') : EMAIL에서 @위치를 찾음
+               --SUBSTR(EMAIL, 1, INSTR(EMAIL, '@')) : EMAIL을 처음부터 @까지 출력함, "아이디+@" 형태로 출력됨
+               --SUBSTR(EMAIL, 1, INSTR(EMAIL, '@')-1) : EMAIL에서 @위치를 찾고, @를 지우고 "아이디"만 출력
+FROM EMPLOYEE
+ORDER BY "이메일 아이디";
+
+/*****그룹 함수*****/
+--1. COUNT : 행의 총 갯수를 셀 때 사용하는 함수
+
+/*
+COUNT 사용 방법
+COUNT(*) : 조회된 모든 행의 개수 반환 -> 이름, 주소와 같은 항목이 몇 개 있는가?
+COUNT(컬럼명) : 지정된 컬럼 값이 NULL이 아닌 행의 개수를 반환
+                NULL은 제외하고 개수를 작성하겠다는 의미
+COUNT(DISTINCT 컬럼명) : 지정된 컬럼에서 중복 값을 제외한 행의 개수 반환
+*/
+
+--EMPLOYEE 테이블 전체 행의 개수
+SELECT COUNT(*)
+FROM EMPLOYEE;--23
+
+--전화번호가 있는 사원의 수
+SELECT COUNT(*)
+FROM EMPLOYEE
+WHERE PHONE IS NOT NULL;--20
+
+--좀 더 가볍게 WHERE을 사용하지 않고 전화번호가 있는 사원의 수
+SELECT COUNT(PHONE)
+FROM EMPLOYEE;--20
+
+--2. AVG : 평균 조회
+--EMPLOYEE 테이블에서 모든 사원의 평균 급여 조회
+SELECT AVG(SALARY) AS "평균 급여"
+FROM EMPLOYEE;
+--D5 부서의 평균 급여 조회
+SELECT AVG(SALARY) AS "D5 부서 평균 급여"
+FROM EMPLOYEE
+WHERE DEPT_CODE  = 'D5';
+--부서별 평균 급여 조회
+SELECT DISTINCT DEPT_CODE AS "부서", AVG(SALARY) AS "평균 급여"
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+/*
+★ 에러 발생
+SELECT DISTINCT DEPT_CODE AS "부서", AVG(SALARY) AS "평균 급여"
+FROM EMPLOYEE;
+★ 오류 zhem
+ORA-00937: not a single-group group function
+00937. 00000 -  "not a single-group group function"
+*Cause:    
+*Action:
+131행, 17열에서 오류 발생
+★ 추후 GROUP BY를 사용해야 함
+*/
+
+/*
+TRIM
+공백 관련 함수
+
+TRIM({[옵션] 문자열|컬럼명 FROM] 문자열|컬럼명)
+
+주어진 문자열의 앞쪽|위쪽|양쪽에 존재하는 지정된 문자열 제거
+-- 옵션 앞 쪽 (LEADING)
+       뒤 쪽 (TRAILING)
+       양 쪽 (BOTH 기본값)
+*/
+
+SELECT '     점     심     ', TRIM(LEADING' 'FROM'     점     심     ') --앞 쪽 공백 제거
+FROM DUAL;--점     심     
+
+SELECT '     점     심     ', TRIM(TRAILING' 'FROM'     점     심     ') --뒤 쪽 공백 제거
+FROM DUAL;--     점     심 : 육안 상으로는 뒤에 공백이 없어진지 모르지만 실제로는 뒤 쪽 공백이 제거되어 있음
+
+SELECT '     점     심     ', TRIM(BOTH' 'FROM'     점     심     ') --양 쪽 공백 제거
+FROM DUAL;--점     심 :
+
+SELECT '     점     심     ', TRIM(' 'FROM'     점     심     ') --(기본값) 양 쪽 공백 제거
+FROM DUAL;--점     심
+
+--앞 쪽, 뒤 쪽, 양 쪽 공백 제거 한번에 사용하기
+SELECT '     점     심     ',
+        TRIM(LEADING' 'FROM'     점     심     '), --앞 쪽 공백 제거
+        TRIM(TRAILING' 'FROM'     점     심     '), --뒤 쪽 공백 제거
+        TRIM(BOTH' 'FROM'     점     심     ') --양 쪽 공백 제거
+FROM DUAL;--점     심
+/*
+★ 오류 발생
+SELECT '     점     심     ', TRIM(LEADING'  'FROM'     점     심     ')
+FROM DUAL;
+★ 해결 방법
+SELECT '     점     심     ', TRIM(LEADING' 'FROM'     점     심     ')
+FROM DUAL;
+===
+TRIM(LEADING' ' ...)
+'' 안에 1개의 글자만 들어가야 한다.
+현재는 '' 안에 띄어쓰기가 2글자 들어가 있음
+★ 오류 코드
+ORA-30001: trim set should have only one character
+30001. 00000 -  "trim set should have only one character"
+*Cause:    Trim set contains more or less than 1 character. This is not
+           allowed in TRIM function.
+*/
+
+/*
+REPLACE : 특정 문자열을 대체할 때 사용
+
+사용방법
+(문자열|컬럼명, 찾을 문자열, 바꿀 문자열)
+*/
+
+--NATIONAL 테이블에서 한국으로 적혀있는 글자를 대한민국으로 변경하기
+SELECT *
+FROM NATIONAL;
+
+SELECT NATIONAL_CODE, NATIONAL_NAME, REPLACE(NATIONAL_NAME, '한국', '대한민국')
+FROM NATIONAL;
---
+++ SQL_코드/02_함수_실습.sql
@@ -0,0 +1,114 @@
+/*함수 실습 코드*/
+
+--LENGTH
+
+--1. 직원 이름의 길이 조회, 이름_길이로 작성
+SELECT EMP_ID, EMP_NAME, LENGTH(EMP_NAME) AS "이름_길이"
+FROM EMPLOYEE;
+
+--2. 이메일의 길이 조회, 이메일_길이로 작성
+SELECT EMP_ID, EMP_NAME, EMAIL, LENGTH(EMAIL) AS "이메일_길이"
+FROM EMPLOYEE;
+
+--3. 전화번호 길이가 11자리인 직원 조회
+SELECT EMP_ID, EMP_NAME, PHONE, LENGTH(PHONE) AS "전화번호_길이"
+FROM EMPLOYEE
+WHERE LENGTH(PHONE) = 11;
+
+--4. 이메일의 길이가 15자 이상인 직원 조회
+SELECT EMP_ID, EMP_NAME, EMAIL, LENGTH(EMAIL) AS "이메일_길이"
+FROM EMPLOYEE
+WHERE LENGTH(EMAIL) >= 15;
+
+--5. 부서코드가 2글자인 직원 조회
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, LENGTH(DEPT_CODE) AS "부서코드_길이"
+FROM EMPLOYEE
+WHERE LENGTH(DEPT_CODE) = 2;
+
+--6. 전화번호 010이 포함된 직원 조회
+SELECT EMP_ID, EMP_NAME, PHONE
+FROM EMPLOYEE
+WHERE INSTR(PHONE, '010') > 0;
+
+--6-1. LIKE로 변경해서 작성
+SELECT EMP_ID, EMP_NAME, PHONE
+FROM EMPLOYEE
+WHERE PHONE LIKE '%010%';
+
+--SUBSTR 예제
+
+--1. 이름의 세 글자 가져오기, AS "이름_석자"
+SELECT EMP_NAME, SUBSTR(EMP_NAME, 1) AS "이름_석자"
+FROM EMPLOYEE;
+
+--2. 전화번호의 마지막 네 자리 가져오기
+SELECT EMP_NAME, SUBSTR(PHONE, -4) AS "마지막_네자리"
+                   ---4 : SUBSTR에서 -를 붙이면 시작부터가 아니라 끝에서부터 4개 가져올 수도 있다
+FROM EMPLOYEE
+WHERE LENGTH(PHONE) >= 11;
+   --LENGTH(PHONE) >= 11 : NULL값이 아닌 번호만 보길 원함
+   --                      WHERE PHONE IS NOT NULL;도 가능
+
+--3. 주민등록번호의 생년월일 추출
+SELECT EMP_NAME, SUBSTR(EMP_NO, 1, 6) AS "생년월일"
+FROM EMPLOYEE;
+
+--4. 전화번호의 앞 세자리 추출
+SELECT EMP_NAME, SUBSTR(PHONE, 1, 3) AS "앞_세자리"
+FROM EMPLOYEE
+WHERE PHONE IS NOT NULL;
+    --PHONE IS NOT NULL : NULL값이 아닌 번호만 보길 원함
+
+--5. 이메일 @ 뒤에 있는 도메인 추출
+SELECT EMP_NAME, SUBSTR(EMAIL, INSTR(EMAIL, '@')+1) AS "도메인"
+FROM EMPLOYEE;
+
+
+--COUNT
+--1. EMPLOYEE 테이블에서 직원 수 조회
+SELECT COUNT(*) AS "총 직원 수"
+FROM EMPLOYEE;
+--2. 부서가 'D9'인 직원 수 조회
+SELECT COUNT(DEPT_CODE) AS "D9 직원 수"
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D9';
+--3. 보너스를 받은 직원 수 조회
+SELECT COUNT(BONUS) AS "보너스 받은 직원 수"
+FROM EMPLOYEE
+WHERE BONUS IS NOT NULL;
+
+--AVG
+--EMPLOYEE 테이블에서 모든 사원의 평균 급여 조회
+SELECT AVG(SALARY)
+FROM EMPLOYEE;
+
+--평균 급여를 올림, 내림, 반올림 활용해서 정수로 출력
+--1. CEIL : 올림
+SELECT CEIL(AVG(SALARY))
+FROM EMPLOYEE;
+--2. FLOOR : 내림
+SELECT FLOOR(AVG(SALARY))
+FROM EMPLOYEE;
+--3. ROUND : 반올림
+SELECT ROUND(AVG(SALARY))
+FROM EMPLOYEE;
+
+/*REPLACE 활용 실습*/
+--1. 이메일 주소에서 모든 점(.)을 하이픈(-)로 대체
+--> EMP_ID, EMAIL AS "수정된 이메일"
+SELECT EMP_NAME, EMP_ID, EMAIL, REPLACE(EMAIL, '.', '-') AS "수정된 이메일"
+FROM EMPLOYEE;
+
+--2. 주민번호에서 하이픈(-)을 빈 문자열 ""로 대체
+--EMP_NAME, EMP_NO, AS "수정된 주민번호"
+SELECT EMP_NAME, EMP_NO, REPLACE(EMP_NO, '-', '') AS "수정된 주민번호"
+FROM EMPLOYEE;
+
+--3. 이름에서 '김'을 'KIM'으로 대체
+--EMP_NAME AS "수정된 이름"
+SELECT EMP_NAME, REPLACE(EMP_NAME, '김', 'KIM') AS "수정된 이름"
+FROM EMPLOYEE;
+
+--4. 직무코드에서 J를 JOB으로 대체
+SELECT EMP_NAME, JOB_CODE, REPLACE(JOB_CODE, 'J', 'JOB') AS "수정된 직무코드"
+FROM EMPLOYEE;
